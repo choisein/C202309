dea57fdc17365e69c6a66d6577cef3e1d589e692
choisein <ssaavv0@gmail.com>
2023-09-13 17:02:35 +0900

    week2practice

---
+++ practice/week2/variable.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+	int x;
+	int y;
+	int z;
+
+	x = 1; //변수에 값을 대입
+	y = 2; //변수에 값을 대입
+
+	z = x + y; // 변수 값을 더한 결과를 변수에 저장
+
+	printf("%d", z); // 정수 z값을 출력
+	
+	return 0;
+}
