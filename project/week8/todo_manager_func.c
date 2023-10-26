9251d4e0a36a8eed56f103aa7bddfe3408f1e5e2
choisein <ssaavv0@gmail.com>
2023-10-27 01:18:56 +0900

    week8. 할일관리

--- project/week8/todo_manager_func.c
+++ project/week8/todo_manager_func.c
@@ -3,9 +3,40 @@
 #define CHAR_NUM 100
 #include <string.h>
 
+char tasks[MAX_TASKS][CHAR_NUM] = { "" }; // 할 일 목록을 저장하기 위한 2차원 배열
+int taskCount = 0; // 할 일의 수를 저장하기 위한 변수
+
+//할 일 추가 함수
+//함수 선언 및 정의 전에 배열과 변수를 설정해줬기 때문에 매개변수가 없는 함수
+void addTask(void) {
+	printf("할 일을 입력하세요 (공백 없이 입력하세요): ");
+	scanf_s("%s", tasks[taskCount], (int)sizeof(tasks[taskCount]));
+	printf("할 일 ""%s""가 저장되었습니다\n\n", tasks[taskCount]);
+}
+//할 일 삭제 함수
+void delTask(int delIndex, int taskCount) {
+	printf("%d. %s : 할 일을 삭제합니다.\n", delIndex, tasks[delIndex - 1]);
+
+	// 배열간 대입 (=배열에 문자 배열인 문자열의 대입) 이 불가능하기 때문에
+	// 문자열 복사 함수로 삭제
+	strcpy_s(tasks[delIndex - 1], sizeof(tasks[delIndex - 1]), "");
+
+	// 특정 인덱스의 할 일 삭제 후 뒤에 있는 할 일 앞으로 옮기기
+	for (int i = delIndex; i < taskCount + 1; i++) {
+		strcpy_s(tasks[i - 1], sizeof(tasks[i]), tasks[i]);
+	}
+}
+//목록 보기 함수
+void printTask(int taskCount) {
+		for (int i = 0; i < taskCount; i++) {
+			printf("%d. %s \n", i + 1, tasks[i]);
+		}
+		printf("\n");
+}
+
+
 int main() {
-	char tasks[MAX_TASKS][CHAR_NUM] = { "" }; // 할 일 목록을 저장하기 위한 2차원 배열
-	int taskCount = 0; // 할 일의 수를 저장하기 위한 변수
+	
 
 	printf("TODO 리스트 시작! \n");
 
@@ -34,9 +65,7 @@ int main() {
 				break;
 				// 할 일이 10개로 다 찬 경우 프로그램 종료
 			}
-			printf("할 일을 입력하세요 (공백 없이 입력하세요): ");
-			scanf_s("%s", tasks[taskCount], (int)sizeof(tasks[taskCount]));
-			printf("할 일 ""%s""가 저장되었습니다\n\n", tasks[taskCount]);
+			addTask();
 			taskCount++;
 			break;
 		case 2:
@@ -45,27 +74,16 @@ int main() {
 			scanf_s("%d", &delIndex);
 			if (delIndex > taskCount || delIndex <= 0) {
 				printf("삭제 범위가 벗어났습니다.\n");
-			}
+			}// 삭제할 할 일의 번호를 잘못 입력했을 때 실행됨
 			else {
-				printf("%d. %s : 할 일을 삭제합니다.\n", delIndex, tasks[delIndex - 1]);
-
-				// 배열간 대입 (=배열에 문자 배열인 문자열의 대입) 이 불가능하기 때문에
-				// 문자열 복사 함수로 삭제
-				strcpy_s(tasks[delIndex - 1], sizeof(tasks[delIndex - 1]), "");
-
-				// 특정 인덱스의 할 일 삭제 후 뒤에 있는 할 일 앞으로 옮기기
-				for (int i = delIndex; i < taskCount + 1; i++) {
-					strcpy_s(tasks[i - 1], sizeof(tasks[i]), tasks[i]);
-				}
+				delTask(delIndex, taskCount);
 				taskCount -= 1;
-			}
+				}	
+			
 			break;
 		case 3:
 			printf("할 일 목록\n");
-			for (int i = 0; i < taskCount; i++) {
-				printf("%d. %s \n", i + 1, tasks[i]);
-			}
-			printf("\n");
+			printTask(taskCount);
 			break;
 		case 4:
 			terminate = 1;
@@ -74,7 +92,7 @@ int main() {
 			//할 일 수정하는 코드블록
 			printf("수정할 할 일의 번호를 입력해주세요. (1부터 시작):");
 			scanf_s("%d", &changeIndex);
-			ch = getchar();
+			ch = getchar(); 
 			if (changeIndex > taskCount || changeIndex <= 0) {
 				printf("수정 범위가 벗어났습니다.\n");
 			}
@@ -89,13 +107,13 @@ int main() {
 			}
 			break;
 		default:
-			printf("잘못된 선택입니다. 다시 선택하세요.\n");
+			printf("잘못된 선택입니다. 다시 선택하세요.\n");  //메뉴에 없는 번호 입력시 실행
 		}
-
+		
 		if (terminate == 1) {
 			printf("종료를 선택하셨습니다. 프로그램을 종료합니다.\n");
 			break;
-		}
+		} 
 		if (terminate == 2) {
 			return 0;
 		}  //할 일이 10개로 다 찬 경우 switch문에서 벗어나 프로그램 종료
